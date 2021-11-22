2b6e00aefb6813e1c4cf79cd111da4ae592ca926
Pascal Brauns <pascal-brauns@protonmail.com>
2021-11-22 19:39:59 +0100

    Fixed action check on dispatched promotion action

--- src/Router/Game.ts
+++ src/Router/Game.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import * as DB from 'DB';
 import IO from 'IO';
 import _ from 'lodash';
-import { Color, Turn } from 'chess-processor';
+import { Color, Turn, Type } from 'chess-processor';
 import * as Pure from 'Pure';
 
 const router = express.Router();
@@ -35,14 +35,18 @@ router.get('/:id/player/:user/color', async (req, res) => {
 });
 
 router.post('/:id/player/:user/action', async (req, res) => {
-  const action = req.body;
+  const action = req.body as Type.Placement;
   const { id } = req.params;
   const user = await DB.User.get(req.params.user);
   const game = await DB.Game.get(id);
   const picks = Turn.picks(game.state.board, game.state.timeline);
   const allowed = picks.some(pick => (
     Turn.placements(game.state.board, game.state.timeline, pick)
-      .some(placement => _.isEqual(placement, action)) &&
+      .some(placement => (
+        placement.type === 'promotion' && action.type === 'promotion'
+          ? _.isEqual(placement.move, action.move)
+          : _.isEqual(placement, action)
+      )) &&
     Color.turn(game.state.timeline) === (
       game.players.find(player => player._id === user._id)?.color
     )
